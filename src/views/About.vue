95d1f859f850ae6ccde960c120adf9ede7ac7ce4
omarchouman <omar.chouman0@gmail.com>
2021-12-06 17:55:49 +0200

    Finished Main Components and Views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <NavBar />
+    <CommonHeroSection title="About" description="about jobly" />
+    <AboutSection />
+    <FooterSection />
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+import CommonHeroSection from "@/components/CommonHeroSection.vue";
+import AboutSection from "@/components/AboutSection.vue";
+// import ResumeSection from "@/components/ResumeSection.vue";
+// import LatestBlogSection from "@/components/LatestBlogSection.vue";
+import FooterSection from "@/components/FooterSection.vue";
+
+export default {
+  name: "About",
+  title: "About | jobly",
+  components: {
+    NavBar,
+    CommonHeroSection,
+    // ResumeSection,
+    AboutSection,
+    // LatestBlogSection,
+    FooterSection,
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,37 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <NavBar />
+    <CategorySection />
+    <FeaturedJobSection />
+    <ResumeSection />
+    <RecentJobSection />
+    <FooterSection />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import NavBar from "@/components/NavBarHero.vue";
+// import HeroSection from "@/components/HeroSection.vue";
+import CategorySection from "@/components/CategorySection.vue";
+import ResumeSection from "@/components/ResumeSection.vue";
+import FeaturedJobSection from "@/components/FeaturedJobSection.vue";
+import RecentJobSection from "@/components/RecentJobSection.vue";
+import FooterSection from "@/components/FooterSection.vue";
 
 export default {
   name: "Home",
+  title: "jobly - Best Online Job Portal in Nepal",
+  mounted() {
+    this.$Progress.finish();
+  },
   components: {
-    HelloWorld,
+    NavBar,
+    CategorySection,
+    ResumeSection,
+    FeaturedJobSection,
+    RecentJobSection,
+    FooterSection,
   },
 };
 </script>
